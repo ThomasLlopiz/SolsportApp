6dcbb267418fe3f4b1538f4f9315dee0cdfec235
ThomasLlopiz <toomillopiz@gmail.com>
2024-08-08 15:45:16 -0300

    estaba bien

--- src/pages/Etapas.jsx
+++ src/pages/Etapas.jsx
@@ -34,6 +34,9 @@ export const Etapas = ({ articuloId, pedidosId }) => {
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
   const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
   const navigate = useNavigate();
+  const [firstDate, setFirstDate] = useState("");
+  const [lastDate, setLastDate] = useState("");
+  const [lastEtapa, setLastEtapa] = useState(null);
 
   useEffect(() => {
     fetchEtapas();
