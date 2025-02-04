9802b349bc22345a2a1149352904fde6d1e4f5dd
ThomasLlopiz <toomillopiz@gmail.com>
2025-02-04 10:00:33 -0300

    rutas privadas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -10,20 +10,25 @@ import { Cotizador } from "../pages/Cotizador";
 import { Cotizacion } from "../pages/Cotizacion";
 import { Telas } from "../pages/Telas";
 import { Agregados } from "../pages/Agregados";
-
+import { PrivateAdminRoute, PrivateUserRoute } from "../hooks/PrivateRoutes";
 export const AppRouter = () => {
   return (
     <Routes>
+      {/* Ruta pública */}
       <Route path="/" element={<Login />} />
       <Route path="/prepage" element={<PrePage />} />
-      <Route path="/pedidos" element={<Pedidos />} />
-      <Route path="/pedidos/:id" element={<Pedido />} />
-      <Route path="/cotizador" element={<Cotizador />} />
-      <Route path="/cotizador/:id" element={<Cotizacion />} />
+      {/* Ruta privada para user (solo /pedidos y /pedidos/:id) */}
+      <Route path="/pedidos" element={<PrivateUserRoute element={<Pedidos />} />} />
+      <Route path="/pedidos/:id" element={<PrivateUserRoute element={<Pedido />} />} />
+      <Route path="/articulos" element={<PrivateUserRoute element={<Articulos />} />} />
+      <Route path="/articulos/:id" element={<PrivateUserRoute element={<Articulos />} />} />
+      {/* Rutas privadas para admin (solo admin puede acceder a estas) */}
+      <Route path="/cotizador" element={<PrivateAdminRoute element={<Cotizador />} />} />
+      <Route path="/cotizador/:id" element={<PrivateAdminRoute element={<Cotizacion />} />} />
       <Route path="/articulos" element={<Articulos />} />
       <Route path="/articulos/:id" element={<Articulo />} />
-      <Route path="/telas" element={<Telas />} />
-      <Route path="/agregados" element={<Agregados />} />
+      <Route path="/telas" element={<PrivateAdminRoute element={<Telas />} />} />
+      <Route path="/agregados" element={<PrivateAdminRoute element={<Agregados />} />} />
     </Routes>
   );
 };
