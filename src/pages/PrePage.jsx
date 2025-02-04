ba606ecb98f966e0a416efc038e262e1863636fa
ThomasLlopiz <toomillopiz@gmail.com>
2025-02-04 09:38:18 -0300

    estilos

--- src/pages/PrePage.jsx
+++ src/pages/PrePage.jsx
@@ -8,7 +8,7 @@ export const PrePage = () => {
     navigate("/");
   };
   return (
-    <div className="flex justify-center items-center gap-32 mt-2 mb-10 bg-gray-100">
+    <div className="flex justify-center items-center gap-32 mt-2 pb-12 bg-gray-100">
       <div className="flex gap-3">
         <button className="bg-blue-500 text-white px-4 py-2 rounded flex">
           <Link to="/cotizador">Ir a Cotizador</Link>
