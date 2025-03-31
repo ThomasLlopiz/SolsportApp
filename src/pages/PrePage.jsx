37d219130757523aed70987e05a3904cfd44be60
ThomasLlopiz <toomillopiz@gmail.com>
2025-03-31 10:50:18 -0300

    cambio estilos

--- src/pages/PrePage.jsx
+++ src/pages/PrePage.jsx
@@ -1,29 +1,22 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const PrePage = () => {
-  const navigate = useNavigate();
-  const handleLogout = () => {
-    localStorage.removeItem("authToken");
-    navigate("/");
-  };
   return (
     <div className="flex justify-center items-center gap-32 mt-2 pb-12 bg-gray-100">
-      <div className="flex gap-3">
+      <div className="flex gap-10">
         <button className="bg-blue-500 text-white px-4 py-2 rounded flex">
-          <Link to="/cotizador">Ir a Cotizador</Link>
+          <Link to="/cotizador">Cotizador</Link>
         </button>
         <button className="bg-blue-500 text-white px-4 py-2 rounded flex">
-          <Link to="/telas">Ir a Telas</Link>
+          <Link to="/telas">Telas</Link>
         </button>
         <button className="bg-blue-500 text-white px-4 py-2 rounded flex">
-          <Link to="/agregados">Ir a Agregados</Link>
+          <Link to="/agregados">Agregados</Link>
         </button>
         <button className="bg-blue-500 text-white px-4 py-2 rounded flex">
-          <Link to="/pedidos">Ir a Pedidos</Link>
+          <Link to="/pedidos">Pedidos</Link>
         </button>
       </div>
-
-
     </div>
   );
 };
