8186e2b59fca1660392c9efb60d5bf06fb96134a
ThomasLlopiz <toomillopiz@gmail.com>
2025-04-10 08:21:19 -0300

    sacando comentario de etapas

--- src/pages/Articulo.jsx
+++ src/pages/Articulo.jsx
@@ -10,6 +10,7 @@ export const Articulo = () => {
   const [articulo, setArticulo] = useState(null);
   const [editedComment, setEditedComment] = useState("");
   const navigate = useNavigate();
+  const API_URL_PDF = import.meta.env.VITE_API_URL_PDF;
 
   useEffect(() => {
     const fetchData = async () => {
@@ -35,32 +36,6 @@ export const Articulo = () => {
     }
   };
 
-  const handleCommentChange = (e) => {
-    setEditedComment(e.target.value);
-  };
-
-  const handleSaveClick = async () => {
-    try {
-      const response = await fetch(`${API_URL}/articulos/${id}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          ...articulo,
-          comentario: editedComment,
-        }),
-      });
-      if (!response.ok) {
-        throw new Error("Error saving comment");
-      }
-      const updatedArticulo = await response.json();
-      setArticulo((prev) => ({ ...prev, comentario: editedComment }));
-    } catch (error) {
-      console.error("Error saving comment", error);
-    }
-  };
-
   if (!articulo) return <div>Loading...</div>;
 
   return (
@@ -71,42 +46,24 @@ export const Articulo = () => {
             <h3 className="text-2xl">Artículo: {articulo.numero_articulo}</h3>
             <h3 className="text-2xl">{articulo.nombre}</h3>
             <h3 className="text-2xl">{articulo.talle}</h3>
+          </div>
+          <div className="flex gap-3 font-semibold text-center bg-gray-500 rounded-md p-1 text-white">
             <a
-              href={`file:C:/Users/DEPIT-1/Desktop/EXTRAS/${articulo.ruta}`}
+              href={`${API_URL_PDF}/storage/${articulo.ruta}`}
               target="_blank"
               rel="noopener noreferrer"
-              className="text-blue-500"
+              className="text-red-400 text-2xl"
             >
-              Abrir en PDF
+              Abrir PDF
             </a>
           </div>
-
           <button
             onClick={handleBackClick}
-            className="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 transition flex items-center"
+            className="bg-gray-500 text-white p-2 rounded hover:bg-gray-600 transition flex items-center text-xl"
           >
             <ArrowLeftIcon className="h-5 w-5 mr-2" />
             Volver
           </button>
-        </div>
-
-        <div className="mt-6">
-          <div className="flex justify-between items-center mb-4">
-            <h2 className="text-xl font-bold mb-4">Comentario</h2>
-            <button
-              onClick={handleSaveClick}
-              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition mt-2"
-            >
-              Guardar
-            </button>
-          </div>
-
-          <textarea
-            value={editedComment}
-            onChange={handleCommentChange}
-            rows="4"
-            className="w-full p-2 border border-gray-300 rounded"
-          />
         </div>
 
         <div className="mt-6">
